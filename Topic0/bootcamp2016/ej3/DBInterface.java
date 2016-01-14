fc87b46d37341f32702f4c393480bd8d2e9f3fb6
Braian Varona <brvarona@gmail.com>
2016-01-13 21:07:00 -0300

    Elimino archivos de la rama Master

--- Topic0/bootcamp2016/ej3/DBInterface.java
+++
@@ -1,9 +0,0 @@
-package bootcam2016.ej3;
-
-import java.sql.ResultSet;
-
-public interface DBInterface {
-
-	public ResultSet executeQuery(String query);
-	public void executeOperation(String operation); // insert, update or delete
-}
